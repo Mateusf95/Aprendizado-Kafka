9d8dec1d3ce1f0bc7e54a47c8679a132ef1c6510
Mateus H M Ferreira <mateushenrique123@live.com>
2025-08-01 21:13:31 -0300

    Refatoração e ajustes

--- ecommerce/src/main/java/org/example/FraudDetectorService.java
+++ ecommerce/src/main/java/org/example/FraudDetectorService.java
@@ -1,12 +1,9 @@
 package org.example;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-
-import java.time.Duration;
-import java.util.Collections;
 import java.util.Properties;
 import java.util.UUID;
 
@@ -14,35 +11,29 @@ import java.util.UUID;
 public class FraudDetectorService {
 
     public static void main(String[] args) {
-        var consumer = new KafkaConsumer<String, String>(properties());
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
-        while (true){
-            var records = consumer.poll(Duration.ofMillis(100));
 
-            if(!records.isEmpty()){
-                System.out.println("Encontrei " + records.count() + " registros!");
-
-                for(var record : records){
-                    System.out.println("----------------------------------------");
-                    System.out.println("Processing new order, checking for fraud");
-                    System.out.println(record.key());
-                    System.out.println(record.value());
-                    System.out.println(record.partition());
-                    System.out.println(record.offset());
-
-                    try{
-                        Thread.sleep(5000);
-                    }catch (InterruptedException e){
-                        // ignoring
-                        e.printStackTrace();
-                    }
-                    System.out.println("Order processed");
-
-                }
-            }
-
-
+        try(var service = new KafkaService(FraudDetectorService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                FraudDetectorService::parse)){
+            service.run();
         }
+    }
+
+    private static void parse(ConsumerRecord<String, String> record) {
+        System.out.println("----------------------------------------");
+        System.out.println("Processing new order, checking for fraud");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+
+        try{
+            Thread.sleep(5000);
+        }catch (InterruptedException e){
+            // ignoring
+            e.printStackTrace();
+        }
+        System.out.println("Order processed");
     }
 
     private static Properties properties() {
--- ecommerce/src/main/java/org/example/NewOrderMain.java
+++ ecommerce/src/main/java/org/example/NewOrderMain.java
@@ -12,34 +12,14 @@ import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var producer = new KafkaProducer<String, String>(properties());
-        for(var i = 0; i< 100; i++){
-
-            var key = UUID.randomUUID().toString();
-            var value = key + "132123,67523,1234";
-            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER",key, value);
-            Callback callback = (data, ex) -> {
-                if (ex != null) {
-                    ex.printStackTrace();
-                    return;
-                }
-                System.out.println("Sucesso enviando " + data.topic() + ":::particion " + data.partition() + "/ offset " + data.offset() + "/ timestamp  " + data.timestamp());
-            };
-            var email = "Thank you for your order! We are processing your order!";
-
-            var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL",key, email);
-            producer.send(record, callback).get();
-
-            producer.send(emailRecord, callback).get();
+        try(var dispatcher = new KafkaDispatcher()) {
+            for(var i = 0; i< 10; i++){
+                var key = UUID.randomUUID().toString();
+                var value = key + "132123,67523,1234";
+                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                var email = "Thank you for your order! We are processing your order!";
+                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+            }
         }
-    }
-
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
-        return properties;
     }
 }
